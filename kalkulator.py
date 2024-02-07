bda0eeb2cc9086a4993e1584c26e7fd9286f9c88
HabibahNurWahidah <158256919+HabibahNurWahidah@users.noreply.github.com>
2024-02-07 13:48:49 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -7,15 +7,16 @@ st.header('Habibah :sparkles:')
 st.subheader('Plot')
 
 # Membuat dua kolom
-c1, c2 = st.columns(2)
+c1, c2, c3= st.columns(3)
 
 # Kolom pertama
 with c1:
-    x = st.number_input('Suhu', value=100)
+    x = st.number_input('Masukkan angka pertama', value=100)
 
 # Kolom kedua
 with c2:
     unit = st.selectbox('Unit', ('C', 'F', 'R', 'K'), key='k1')
+
 
 # Menampilkan hasil input
 st.write(x, unit)
