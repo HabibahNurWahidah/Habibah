b1f0363492eb5e93176f0e26323cc95d8a4338ac
HabibahNurWahidah <158256919+HabibahNurWahidah@users.noreply.github.com>
2024-02-07 13:38:42 +0700

    Create kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# Header
+st.header('Habibah:sparkles:')
+st.subheader('Plot')
+
+c1, c2 = st.columns(2)
+
+with c1:
+  x = st.number_input('Suhu',value=100)
+  st.write('=>:')
+with c2:
+  satuan = st.selectbox(
+    ('C', 'F', 'R', 'K'),key='k1')
+
+sr.write(x,' ',satuan,'= ',' ')
+
