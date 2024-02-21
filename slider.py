a6f87081ca4ec0c30faa58d1c13f4ee6a2edb35e
HabibahNurWahidah <158256919+HabibahNurWahidah@users.noreply.github.com>
2024-02-21 14:03:03 +0700

    Update slider.py

--- slider.py
+++ slider.py
@@ -7,16 +7,23 @@ st.write('nilai x:', x)
 y = st.slider('Set nilai',0.0, 10.0, 5.0)
 st.write('nilai y:', y)
 
-t = np.linspace(x[0]*np.pi,x[1]*np.pi,100)
+t = np.linspace(x[0]*np.pi, x[1]*np.pi, 100)
 u = np.sin(y*t)
-#st.write('nilai t:', t)
+
+# Calculate f(x) = 2x^2 + 9x - 2
+def f(x):
+    return 2*x**2 + 9*x - 2
+
+v = f(t)
 
 fig, ax = plt.subplots(figsize=(16, 8))
-ax.plot(t, u, label='sin(t)', color='b') #plotting sin(t) curve
+ax.plot(t, u, label='sin(t)', color='b')  # Plotting sin(t) curve
+ax.plot(t, v, label='f(t)', color='r')     # Plotting f(t) curve
 ax.set_ylabel("")
 ax.set_xlabel("t")
 ax.tick_params(axis='y', labelsize=20)
 ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
 ax.tick_params(axis='x', labelsize=15)
 plt.grid(color='green', linestyle='-.', linewidth=.5)
+plt.legend()  # Show legend
 st.pyplot(fig)
